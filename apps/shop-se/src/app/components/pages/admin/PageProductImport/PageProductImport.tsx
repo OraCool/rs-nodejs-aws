2eff184e9e3b8c282d8545434fb1a2334a29a9bf
OraCool <sOraCool@gmail.com>
2020-11-14 17:00:06 +0300

    erged with upstream repository

--- apps/shop-se/src/app/components/pages/admin/PageProductImport/PageProductImport.tsx
+++ apps/shop-se/src/app/components/pages/admin/PageProductImport/PageProductImport.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import API_PATHS from '../../../../constants/apiPaths';
 import CSVFileImport from './components/CSVFileImport';
 import ProductsTable from './components/ProductsTable';
@@ -20,7 +20,7 @@ export default function PageProductImport() {
   return (
     <div className={classes.content}>
       <Box display='flex' alignItems='center'>
-        <CSVFileImport url={`${API_PATHS.import}/product/import`} title='Import Products CSV'/>
+        <CSVFileImport url={`${API_PATHS.import}/import`} title='Import Products CSV'/>
         <Button size='small' color='primary' variant='contained' component={Link} to={'/admin/product-form/'}>
           create product
         </Button>
